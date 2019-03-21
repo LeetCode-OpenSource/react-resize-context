2f9f5bc62d5eb1c70e59ee1810195c0692746c22
Runjuu <huang@runjuu.com>
2019-03-21 19:26:46 +0800

    feat: export context

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { default as ResizeProvider } from './ResizeProvider';
 export { default as ResizeConsumer } from './ResizeConsumer';
 export * from './types';
 export * from './hooks';
+export * from './context';
 export * from './renderWithSize';
